7d1baad5158d14ba24f17e1398e04e1b0e653f7f
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2019-10-29 11:22:30 -0700

    starter code

---
+++ poetry.py
@@ -0,0 +1,22 @@
+poem = ""
+
+
+
+
+def lines_printed_backwards(lines_list):
+
+    # TOSO: Reverse the loop
+    lines_list.reverse()
+    print(lines_list)
+    # TODO: use loop to print out items in list
+    pass
+
+def lines_printed_random():
+    pass
+
+
+def my_costum_function():
+    pass
+
+lines_list = poem.split("\n")
+print(lines_list)
